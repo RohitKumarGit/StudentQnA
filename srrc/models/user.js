5c45f3f668410edcbd19e77f8bd408ccd5fc90a6
Rohit Kumar <campon99@gmail.com>
2020-03-16 18:18:21 +0530

    add all the back end APIs

---
+++ srrc/models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+    name : String,
+    email:String,
+    role:String,
+    college:String,
+    profileUrl:String,
+    points : {
+        type:Number,
+        default:500
+    }
+});
+
+userSchema.statics.getUser = async function(email){
+    console.log(email)
+    const user =await User.findOne({email});
+    console.log(user)
+    return user;
+}
+userSchema.statics.createUser = async function(details){
+    const user = new User(details);
+    await user.save()
+}
+userSchema.statics.deductPoint = async function(email,point){
+   
+    const user =await User.findOne({email});
+    console.log(user)
+    console.log(point)
+    const x = user.points - point
+
+    user.points = x;
+    console.log(user)
+    await user.save()
+}
+const User =new  mongoose.model('User',userSchema);
+module.exports = User
