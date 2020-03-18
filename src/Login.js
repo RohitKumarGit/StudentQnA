dbcd26309a779fc2fef9f3e84b453b3259ef2a52
Ricerati <58187033+ricerati@users.noreply.github.com>
2020-03-18 12:28:11 +0100

    Add validation before authorizing

--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,61 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-const axios = require('axios');
-const firebase = require('firebase');
+const axios = require("axios");
+const firebase = require("firebase");
 class Login extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       email: "",
-      password: ""
+      emailErr: "",
+      password: "",
+      passwordErr: "",
+      formErr: ""
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  validateForm() {}
+  validateForm(form, type) {
+    let valid = true;
+
+    if (type === "all" || type === "email") {
+      let emailErr = "";
+
+      // Check if no email has been set
+      if (form.email.value.length === 0) {
+        emailErr = "Please enter your email.";
+        valid = false;
+      } else {
+        let re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if (!re.test(form.email.value.toLowerCase())) {
+          emailErr = "Please enter a valid email.";
+          valid = false;
+        }
+      }
+
+      this.setState({
+        emailErr: emailErr
+      });
+    }
+
+    if (type === "all" || type === "password") {
+      let passwordErr = "";
+
+      if (form.password.value.length === 0) {
+        passwordErr = "Please enter your password.";
+        valid = false;
+      }
+
+      this.setState({
+        passwordErr: passwordErr
+      });
+    }
+
+    return valid;
+  }
 
   handleChange(event) {
     const target = event.target;
@@ -30,66 +70,90 @@ class Login extends Component {
       ".form__helper"
     );
 
-    switch (name) {
-      case "email":
-        if (value.length === 0) {
-          formHelperElement.innerText = "Please enter your username.";
-          target.setCustomValidity("Please enter your username.");
-        } else {
-          formHelperElement.innerText = "";
-          target.setCustomValidity("");
-        }
+    // console.log(event.target);
 
-        break;
+    this.validateForm(event.target.form, name);
 
-      case "password":
-        if (value.length === 0) {
-          formHelperElement.innerText = "Please enter your password.";
-          target.setCustomValidity("Please enter your password.");
-        } else {
-          formHelperElement.innerText = "";
-          target.setCustomValidity("");
-        }
+    // switch (name) {
+    //   case "email":
+    //     if (value.length === 0) {
+    //       formHelperElement.innerText = "Please enter your username.";
+    //       target.setCustomValidity("Please enter your username.");
+    //     } else {
+    //       formHelperElement.innerText = "";
+    //       target.setCustomValidity("");
+    //     }
 
-        break;
-    }
+    //     break;
+
+    //   case "password":
+    //     if (value.length === 0) {
+    //       formHelperElement.innerText = "Please enter your password.";
+    //       target.setCustomValidity("Please enter your password.");
+    //     } else {
+    //       formHelperElement.innerText = "";
+    //       target.setCustomValidity("");
+    //     }
+
+    //     break;
+    // }
   }
 
   handleSubmit(event) {
     event.preventDefault();
 
-    // TODO: Check if form is valid
-    // TODO: Attempt to authorize user
+    const emailElement = event.target.email;
+    const passwordElement = event.target.password;
+
     const email = event.target.email.value;
-    firebase.auth().signInWithEmailAndPassword(event.target.email.value, event.target.password.value).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ...
-      console.log(errorCode,errorMessage);
-    });
-    const set = function(data){
-      
+    const password = event.target.password.value;
+
+    // Check if form is valid
+    if (!this.validateForm(event.target, "all")) {
+      return false;
     }
-    const m = this
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        axios.get('/user/'+email).then((response)=>{
-          console.log("logged")
-          set(response.data)
-          console.log(response.data) // if the user is logged response.data has all the user details
-          m.setState({user:response.data})
-        }).catch((e)=>{
-          console.log(e)
+
+    // Attempt to authorize user
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailElement.value, passwordElement.value)
+      .catch(err => {
+        let errCode = err.code;
+        let formErr = "";
+
+        if (
+          errCode === "auth/invalid-email" ||
+          errCode === "auth/user-not-found" ||
+          errCode === "auth/wrong-password"
+        ) {
+          formErr = "Username or password is incorrect.";
+        } else {
+          formErr = "An unexpected error occured.";
+          console.log(err.code);
+        }
+
+        this.setState({
+          formErr: formErr
         });
-      } 
-      else{
+      });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        axios
+          .get("/user/" + email)
+          .then(response => {
+            console.log("logged");
+            // set(response.data);
+            console.log(response.data); // if the user is logged response.data has all the user details
+            this.setState({ user: response.data });
+          })
+          .catch(e => {
+            console.log(e);
+          });
+      } else {
         // if user is not logged
       }
     });
-    console.log(
-      `The form was submitted: ${this.state.email} - ${this.state.password}`
-    );
   }
 
   render() {
@@ -101,7 +165,7 @@ class Login extends Component {
           </header>
 
           <div class="form__group">
-            <label class="form__label">Email or username</label>
+            <label class="form__label">Email</label>
             <input
               class="form__input"
               type="text"
@@ -109,7 +173,7 @@ class Login extends Component {
               value={this.state.email}
               onChange={this.handleChange}
             />
-            <span class="form__helper"></span>
+            <span class="form__helper">{this.state.emailErr}</span>
           </div>
 
           <div class="form__group">
@@ -121,7 +185,7 @@ class Login extends Component {
               value={this.state.password}
               onChange={this.handleChange}
             />
-            <span class="form__helper"></span>
+            <span class="form__helper">{this.state.passwordErr}</span>
           </div>
 
           <div class="form__group">
@@ -134,6 +198,7 @@ class Login extends Component {
 
           <div class="form__group">
             <input class="btn" type="submit" value="Login" />
+            <span class="form__helper">{this.state.formErr}</span>
           </div>
 
           <p class="form__info">
