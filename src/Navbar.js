7c753ebad7a90e3393a5873fb56aa64a2e53f6c7
Ricerati <58187033+ricerati@users.noreply.github.com>
2020-03-25 17:40:35 +0100

    Convert navbar to Semantic UI React

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,51 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
 
 export default function Navbar() {
   return (
-    <div class="ui menu">
-      {/* <div class="ui container"> */}
-      <div class="header item">Student Q&A</div>
-      <div class="right menu">
-        <Link to="/" class="item">
+    <Menu>
+      <Menu.Item header>Student Q&A</Menu.Item>
+      <Menu.Menu position="right">
+        <Link to="/" className="item">
           Home
         </Link>
-        <Link to="/login" class="item">
+        <Link to="/login" className="item">
           Login
         </Link>
-        <Link to="/signup" class="item">
+        <Link to="/signup" className="item">
           Sign Up
         </Link>
-        {/* </div> */}
-      </div>
-    </div>
-    // <header class="navbar-wrapper">
-    //   <style>
-    //     @import
-    //     url('https://fonts.googleapis.com/css?family=Cabin:500&display=swap');
-    //   </style>
-    //   <nav class="ui container navbar">
-    //     <Link to="/" class="navbar__title">
-    //       Title
-    //     </Link>
-    //     <ul class="navbar__items">
-    //       <li class="navbar__item">
-    //         <Link to="/" class="navbar__link">
-    //           Home
-    //         </Link>
-    //       </li>
-    //       <li class="navbar__item">
-    //         <Link to="/login" class="navbar__link">
-    //           Login
-    //         </Link>
-    //       </li>
-    //       <li class="navbar__item">
-    //         <Link to="/signup" class="navbar__link">
-    //           Sign up
-    //         </Link>
-    //       </li>
-    //     </ul>
-    //   </nav>
-    // </header>
+      </Menu.Menu>
+    </Menu>
   );
 }
