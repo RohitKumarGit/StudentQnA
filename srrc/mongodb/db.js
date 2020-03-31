79cd6c560d3d1cff061874beb1f5333f7dbec08e
Ricerati <58187033+ricerati@users.noreply.github.com>
2020-03-31 15:07:10 +0200

    Update Atlas password

--- srrc/mongodb/db.js
+++ srrc/mongodb/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb+srv://root:ZbB6OhSmZ8YSJbo2ZbB6OhSmZ8YSJbo2@cluster0-wkbbu.mongodb.net/test",
+  "mongodb+srv://root:PpfaEyRSi5xiDaL@cluster0-wkbbu.mongodb.net/test",
   {
     useNewUrlParser: true,
     useCreateIndex: true
