9f237f6778e4c30cfd2a62f379cf836ad4be3734
Ricerati <58187033+ricerati@users.noreply.github.com>
2020-04-04 17:48:43 +0200

    Move CSS for PostInfo to seperate file

--- src/components/PostInfo.js
+++ src/components/PostInfo.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Header, Image } from "semantic-ui-react";
 
+import "./PostInfo.css";
+
 const PostInfo = (props) => {
   const { src, name, age } = props;
 
   return (
-    <Header as="h4" className="post__meta">
+    <Header className="post-meta" as="h4">
       <Image className="post-avatar" circular src={src} size="large" />
       <Header.Content>
         <div className="post-author">{name}</div>
         <div className="post-age">{age}</div>
-        {/* <Header.Subheader>{age}</Header.Subheader> */}
       </Header.Content>
     </Header>
   );
